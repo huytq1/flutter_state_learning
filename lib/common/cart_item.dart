03766d86fed1fc9518881c6020cd7da19dd1c407
huytran84 <htran84@csc.com>
2019-01-04 14:39:40 +0700

    complete common

---
+++ lib/common/cart_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_state_learning/common/product.dart';
+
+class CartItem {
+  final int count;
+  final Product product;
+
+  const CartItem(this.count, this.product);
+
+  @override
+  String toString() => "${product.name} X $count";
+}
